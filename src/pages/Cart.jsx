441ea6d92e7b4cd42916ae3949497cc5023c5e5b
Muhammed <faraanshaik19@gmail.com>
2025-04-11 16:00:10 +0530

    cart page updated

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 
-const Cart = () => {
+const Cart = ({ cart, setCart }) => {
+  console.log(cart);
+
   return (
     <>
-      <h1>Cart Page</h1>
+      <div className="">
+        {cart.length != 0 ? (
+          cart.map((item) => (
+            <div key={item.id}>
+              <div className="">
+                <img
+                  src={item.images[0]}
+                  alt={item.title}
+                  className="w-full h-full object-cover"
+                />
+                <h3>{item.title}</h3>
+                <p>{item.description}</p>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div>Cart is empty</div>
+        )}
+      </div>
     </>
   );
 };
