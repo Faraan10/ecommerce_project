6f8edcaf63161b498a2ded8bafd165dc2a4e4aeb
Muhammed <faraanshaik19@gmail.com>
2025-04-21 22:58:07 +0530

    notfound page updated

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>NotFound</h1>
+    <div className="pt-50 pb-16 px-4 text-center bg-base-100 text-base-content min-h-screen">
+      <h1 className="text-6xl font-bold mb-4">404</h1>
+      <h2 className="text-2xl font-semibold mb-2">Page Not Found</h2>
+      <p className="mb-6 max-w-md mx-auto">
+        Oops! The page you're looking for doesn't exist or has been moved.
+      </p>
+      <Link to="/">
+        <button className="btn btn-primary">Go Back Home</button>
+      </Link>
     </div>
   );
 };
