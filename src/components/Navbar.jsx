899d031cfb59de8baecdd7689d2f8629348a3ada
Muhammed <faraanshaik19@gmail.com>
2025-07-02 23:19:37 +0530

    navbar commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Menu, X, ShoppingCart } from "lucide-react";
 import { Link, useNavigate } from "react-router-dom";
 import AvatarDropdown from "./AvatarDropdown";
